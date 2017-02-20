0d3d368880286c745aef126774a0da63decd968e
Brianmacranaill <Brian.reynolds@mycit.ie>
2017-02-20 21:25:33 +0000

    Add files via upload

---
+++ MainPage.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Home</title>
+		<link rel="stylesheet" href="main.css">
+	</head>
+	
+	<body class="body">
+		<header>
+			<h1 class="PageTitle">
+				<nav>
+					<a href="MainPage.php.php">Automobile Management & Security System</a>
+				</nav>
+			</h1>
+		</header>
+		<br><br>
+		<div>
+			<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean semper varius diam, quis interdum risus rutrum eget. Proin fringilla, tortor eget lobortis dignissim, libero leo facilisis eros, congue sagittis ante lacus ut risus. Etiam mollis tellus in vestibulum mollis. In sit amet augue finibus, eleifend massa feugiat, eleifend quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse sit amet justo lobortis, ultrices lorem id, elementum massa. Quisque vel massa dignissim, vehicula lacus et, aliquam lectus. Sed pulvinar sapien magna, quis elementum leo fringilla non. Vivamus gravida ornare quam ut elementum. Nullam in commodo nisi. Phasellus eros enim, molestie in tempor sed, egestas id dui. Fusce dapibus ex at magna mollis viverra. Aenean sit amet mauris facilisis, varius metus nec, fermentum lectus. </h2>
+		</div>
+		<br><br><br><br><br><br>
+		<div id="3columndiv">
+			<div id="leftdiv">
+				<h1 id="texttitle">Left Title</h1>
+				<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nisi mauris. Nam vel massa quam. Nunc vel maximus dui, at consectetur dolor. Vivamus quis lacus ac ex rutrum sollicitudin porttitor nec elit. Curabitur sagittis venenatis nulla, condimentum maximus ipsum mollis nec. Cras sit amet posuere ante. Integer sit amet varius dolor. Aliquam erat volutpat. Nam consequat mollis risus vel sagittis. Etiam ullamcorper ante vitae leo iaculis, nec euismod magna elementum. Aliquam in eros ante. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ullamcorper diam vel laoreet volutpat. Etiam euismod consequat euismod. Quisque auctor ullamcorper euismod. Nullam ac mattis. </h2>
+				<form action="page2.php">
+					<input type="submit" value="Page2"/>
+				</form>
+			</div>
+			<div id="middiv">
+				<h1 id="texttitle">Middle Title</h1>
+				<h2>Vivamus sed egestas nibh. Phasellus arcu libero, porta aliquam lacinia eu, congue nec nisi. Cras accumsan at lacus id auctor. Quisque congue leo id mi fermentum, eu lobortis ex maximus. Cras non auctor lacus. Quisque facilisis pretium dolor, sit amet gravida eros porttitor id. Nulla nec tincidunt orci. Aenean magna velit, egestas eu finibus non, placerat ac lacus. Mauris non sem felis. Maecenas eget tincidunt odio. Vestibulum justo risus, ullamcorper ac risus nec, suscipit commodo nunc. Phasellus quis cursus orci, ac imperdiet lectus. In eget suscipit lacus. In congue velit nec ligula mattis tristique. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Cras tempus diam vitae tellus faucibus ultricies. Sed tempus, sem vitae consequat semper, nulla augue lacinia ante, ut euismod risus eros non neque. Fusce sodales egestas rutrum. Quisque dignissim tortor ac dui venenatis imperdiet. Etiam id rutrum diam. Curabitur eget tincidunt dolor, sed sagittis neque. Morbi tellus purus, interdum ut eleifend eget, consectetur dignissim libero. Praesent vitae felis ante. </h2>
+				<form action="page3.php">
+					<input type="submit" value="Page3"/>
+				</form>
+			</div>
+			<div id="rightdiv">
+				<h1 id="texttitle">Right Title</h1>
+				<h2>Sed tempus, sem vitae consequat semper, nulla augue lacinia ante, ut euismod risus eros non neque. Fusce sodales egestas rutrum. Quisque dignissim tortor ac dui venenatis imperdiet. Etiam id rutrum diam. Curabitur eget tincidunt dolor, sed sagittis neque. Morbi tellus purus, interdum ut eleifend eget, consectetur dignissim libero. Praesent vitae felis ante. Nullam quam odio, hendrerit non porta ac, vestibulum sit amet ex. Duis et ex ac magna interdum condimentum. Aenean porta sollicitudin sollicitudin. Nulla laoreet tempus felis at sodales. Donec in arcu ac quam rutrum pretium in quis diam. Vivamus ut consequat nunc, nec commodo turpis. Phasellus sed libero lacus. Aenean sed velit arcu. </h2>
+				<form action="page4.php">
+					<input type="submit" value="Page4"/>
+				</form>
+			</div>
+		</div>
+	
+	</body>
+</html>
---
+++ page3.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>page2</title>
+		<link rel="stylesheet" href="main.css">
+	</head>
+	
+	<body class="body">
+		<header>
+			<h1 class="PageTitle">
+				<nav>
+					<a href="MainPage.php.php">Automobile Management & Security System</a>
+				</nav>
+			</h1>
+		</header>
+		<h1>Page3 Title</h1>
+	
+	</body>
+</html>
