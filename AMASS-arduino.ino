2f88b231d61057ff731894f4eaf5dabed9247052
rocallaghan <richard.m.ocallaghan@mycit.ie>
2017-03-06 22:08:26 +0000

    Update AMASS-arduino.ino
    Properly functioning now with the firebase db.

--- AMASS-arduino.ino
+++ AMASS-arduino.ino
@@ -5,10 +5,16 @@
 #define INCLUDE_INTERNET_SHIELD
 #define INCLUDE_TERMINAL_SHIELD
 
+
+
 /* Include 1Sheeld library. */
+
 #include <OneSheeld.h>
 
+
+
 /* A name for the LED on pin 13. */
+
 int ledPin = 13;
 boolean isent = false;
 boolean isMessageSent = false;
@@ -18,64 +24,99 @@ char charlat [10];
 char charlon [10];
 char readings [80];
 char readings1 [80];
-char name = "http://richardmocallaghan.000webhostapp.com/firebaseTest.php?arduino_data=";
-HttpRequest request (name);
+HttpRequest requestx ("http://richardmocallaghan.000webhostapp.com/firebaseTest.php?arduino_data=");
+HttpRequest requesty ("http://richardmocallaghan.000webhostapp.com/firebaseTesty.php?arduino_data=");
+
+
+
+
 
 
 void setup()
 {
   /* Start communication. */
   OneSheeld.begin();
-
   /* Set the LED pin as output. */
   pinMode(ledPin,OUTPUT);
+
 }
+
 
 void loop()
 {
   /* Check X-axis acceleration. */
   if(AccelerometerSensor.getX() > 8)
   {
+
     digitalWrite(ledPin,HIGH);
     Camera.setFlash(ON);
     Camera.frontCapture();
     isent = true;
-   
+
+    
       if(isent = true)
       {
+
       Facebook.postLastPicture("This person has stolen my car of reg 12345");
+
       isent = false;
+
       }
+
+
 
       if (!isMessageSent)
       {
-       
+
         lat = GPS.getLatitude(); lon = GPS.getLongitude();
+
         
         dtostrf(lat, 7, 3, charlat); dtostrf(lon, 7, 3, charlon);
         strcat(readings, "GPS Coordinates of your car : \n");
         strcat(readings, "latitude is : "); strcat (readings, charlat);
         strcat(readings, "\nLongitude is : "); strcat (readings, charlon);
-        strcat (name,charlon);
-        Internet.performGet(request);
+
+        requestx.addParameter("arduino_data",charlon);  
+        requesty.addParameter("arduino_data",charlat);  
+        Internet.performGet(requestx);
+        Internet.performGet(requesty);
+      
+       
+       
         SMS.send("0863967689", readings);
+
      
+
         
+
         
+
         isMessageSent = true;
+
         
+
       }
+
       else
+
       {
+
          isMessageSent = false;
+
       }
+
        OneSheeld.delay(5000);
+
    
-  }
-  else
-  {  
-    digitalWrite(ledPin,LOW);
+
   }
 
-} /* Y axis maybe???? keeps glitching on phone and freaking out causing mis readings?? */
+  else
+
+  {  
+
+    digitalWrite(ledPin,LOW);
+
+  }
+}
  
